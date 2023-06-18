8721d59c512eee5c1be2a3cc80df6cae5d1628eb
JimmyTrouble <114571635+JimmyTrouble@users.noreply.github.com>
2023-06-18 13:34:55 +1000

    main

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import YTSearch from 'youtube-api-search';
+import React, { useState, useEffect } from "react";
+import YTSearch from "youtube-api-search";
 // Components
-import SearchBar from './components/SearchBar/SearchBar';
-import VideoList from './components/VideoList/VideoList';
-import VideoDetail from './components/VideoDetail/VideoDetail';
-import Nav from './components/Nav/Nav';
+import SearchBar from "./components/SearchBar/SearchBar";
+import VideoList from "./components/VideoList/VideoList";
+import VideoDetail from "./components/VideoDetail/VideoDetail";
+import Nav from "./components/Nav/Nav";
 // styles
-import { Container } from './appStyles.js';
+import { Container } from "./appStyles.js";
 // Personal Key, it would be hidden for you. Get One!
-import API_KEY from './key';
+import API_KEY from "./key";
 
 function App() {
   const [videos, setVideos] = useState(null),
     [selectedVideo, setSelectVideo] = useState();
 
-  const handleSearch = term =>
-    YTSearch({ key: API_KEY, term }, videos => {
+  const handleSearch = (term) =>
+    YTSearch({ key: API_KEY, term }, (videos) => {
       setVideos(videos);
       setSelectVideo(videos[0]);
     });
 
   useEffect(() => {
-    handleSearch('Guns and roses');
+    handleSearch("Chon");
   }, []);
 
   return (
@@ -32,7 +32,7 @@ function App() {
       <VideoDetail videos={selectedVideo}>
         <VideoList
           videos={videos}
-          handleSelectVideo={selectedVideo => setSelectVideo(selectedVideo)}
+          handleSelectVideo={(selectedVideo) => setSelectVideo(selectedVideo)}
         />
       </VideoDetail>
     </Container>
